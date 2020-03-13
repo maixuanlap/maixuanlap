bd625e5691979735444975971f0199313a3bd49d
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-03-13 22:03:11 +0700

    Lap02

--- lap02/TestRactangle.java
+++ lap02/TestRactangle.java
@@ -17,7 +17,7 @@ public class TestRactangle {
        c2.getLenght();
         System.out.printf("");
        
-        System.out.printf("\n ");
+        System.out.printf("");
      
         for(int i=1;i<= 3 ;i++ ){
             for(int j = 1; j<=7;j++){
