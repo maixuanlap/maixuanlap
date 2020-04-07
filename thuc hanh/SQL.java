570017340d3a8967f9f0aa8186356cd20931437c
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-04-07 17:17:45 +0700

    thực hành

--- thuc hanh/SQL.java
+++ thuc hanh/SQL.java
@@ -4,6 +4,4 @@ create table student(
         address varchar(50),
         phone varchar(20),
         PRIMARY KEY (student_id));
-        INSERT INTO student (Student_id,student_name,Address,Phone) VALUES('B01010','Nguyen Tuan Anh ','Ha Noi' ,0904818238);
-        INSERT INTO student(Student_id,student_name,Address,Phone) VALUES('B10394',' Nguyen Hoang Hai' ,'Hai Duong', 0494949494);
-      
+        
