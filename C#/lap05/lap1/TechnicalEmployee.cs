60a640b059199dd8b6a9b128c5885625b7d4c07d
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-20 16:36:55 +0700

    Lap05

---
+++ C#/lap05/lap1/TechnicalEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lap1
+{
+    class TechnicalEmployee : Employee
+    {
+        public TechnicalEmployee(String name) : base(name, 75000)
+        {
+
+        }
+        public int successfullCheckIns = 5;
+        public override string employeeStatus()
+        {
+            return this.toString() + "has" + this.successfullCheckIns + "successfull check ins";
+        }
+    }
+}
