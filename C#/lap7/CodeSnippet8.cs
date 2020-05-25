99c7e179d7c39047adfb2fe16ef1e0494fea7d47
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-25 21:41:18 +0700

    lap7

---
+++ C#/lap7/CodeSnippet8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet8
+{
+    class CodeSnippet8
+    {
+        public string Name = "Wilson";
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ C#/lap7/CodeSnippet9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet9
+{
+    class CodeSnippet9
+    {
+        protected float salary;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
