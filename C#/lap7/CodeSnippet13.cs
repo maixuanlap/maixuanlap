e9160ac98493ac3cfcca3f8aa5648af5d5dfd163
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-25 21:38:27 +0700

    lap7

---
+++ C#/lap7/CodeSnippet13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cone_Snippet13
+{
+    class CodeSnippet13
+    {
+        static void Depreciation(out int val)
+        {
+            val = 20000;
+            int dep = val * 5 / 100;
+            int amt = val - dep;
+            Console.WriteLine("DepreciationAmount:" + dep);
+            Console.WriteLine("Reduced value after depreciation: " + amt);
+        }
+        static void Main(string[] args)
+        {
+            int value;
+            Depreciation(out value);
+        }
+    }
+}
