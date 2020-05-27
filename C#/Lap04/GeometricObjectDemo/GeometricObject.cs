3f6d6398d471fd5947950cfb84fd3bf5fa77d099
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-27 22:53:14 +0700

    Lap04

---
+++ C#/Lap04/GeometricObjectDemo/GeometricObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjectDemo
+{
+    public abstract class GeometricObject
+    {
+        protected string color;
+        protected double weight;
+      
+        protected GeometricObject()
+        {
+            color = "white";
+            weight = 1.0;
+        }
+        protected GeometricObject(string color, double weight)
+        {
+            this.color = color;
+            this.weight = weight;
+        }
+      
+        public string PColor
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public double PWeight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+       
+        public abstract double findArea();
+        public abstract double findPerimeter();
+    }
+}
