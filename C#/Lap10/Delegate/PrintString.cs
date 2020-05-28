26e843fa0edaab0f45687ae77a7eb19b79ad70cc
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-28 16:06:05 +0700

    Lap10

---
+++ C#/Lap10/Delegate/PrintString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintString
+{
+    class PrintString
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        public delegate void printString(string s);
+
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine("The String is: {0}", str);
+        }
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream("c:\\message.txt", FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine();
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        public static void sendString(printString ps)
+        {
+            ps("Hello World");
+        }
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            sendString(ps1);
+            sendString(ps2);
+            Console.ReadKey();
+        }
+    }
+}
+
