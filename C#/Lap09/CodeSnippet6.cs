e74914a707cc06b4314860167056a21d0c2ce47c
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-05-29 11:53:09 +0700

    Lap09

---
+++ C#/Lap09/CodeSnippet6.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippet_6
+{
+    class CodeSnippet6
+    {
+        interface IDetails
+        {
+            void GetDetails();
+        }
+        class Student : IDetails
+        {
+            string studName;
+            int studID;
+            public Student(string name, int num)
+            {
+                studName = name;
+                studID = num;
+            }
+            public void GetDetails()
+            {
+                Console.WriteLine(studID + "\t" + studName);
+            }
+            class GenericList<T> where T : IDetails
+            {
+                T[] values = new T[3];
+                int counter = 0;
+                public void Add(T val)
+                {
+                    values[counter] = val;
+                    counter++;
+                }
+                public void Display()
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        values[i].GetDetails();
+                    }
+
+                }
+            }
+
+            class InterfaceConstraintDemo
+            {
+                static void Main(string[] args)
+                {
+                    GenericList<Student> objList = new GenericList<Student>();
+                    objList.Add(new Student("Wilson", 120));
+                    objList.Add(new Student("Jack", 130));
+                    objList.Add(new Student("Peter", 140));
+                    objList.Display();
+                }
+            }
+        }
+    }
+}
+    
+
+
---
+++ C#/Lap09/CodeSnippet8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippet8
+{
+    class General<T, U>
+    {
+        T valOne;
+        U valTwo;
+        public void AcceptValues(T item)
+        {
+            valOne = item;
+        }
+        public void AcceptValues(U item)
+        {
+            valTwo = item;
+        }
+        public void Display()
+        {
+            Console.Write(valOne + "\t" +valTwo);
+        }
+    }
+    class MethodOverload
+    {
+        static void Main(string[] args)
+        {
+            General<int, string> objGenOne = new General<int, string>();
+            objGenOne.AcceptValues(10);
+            objGenOne.AcceptValues("Smith");
+            Console.WriteLine("ID\tName\tDesignation\tSalary");
+            objGenOne.Display();
+            General<string, float> objGenTwo = new General<string, float>();
+            objGenTwo.AcceptValues("Mechanic");
+            objGenTwo.AcceptValues(2500);
+            Console.Write("\t");
+            objGenTwo.Display();
+            Console.WriteLine();
+        }
+    }
+}
