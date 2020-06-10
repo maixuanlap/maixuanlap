72864f789f409a4cdbedc120f32869591607c235
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-06-10 21:21:25 +0700

    Lap

---
+++ PHP/Lap04/Session8/CodeSnippet9.php
@@ -0,0 +1,16 @@
+<?php
+$a=30;
+$b=6;
+$a+=$b;
+echo"The value of $a+=$b is:".$a."<br>";
+$a-=$b;
+echo"The value of $a-=$b is:".$a."<br>";
+$a*=$b;
+echo"The value of $a*=$b is:".$a."<br>";
+$a/=$b;
+echo"The value of $a/=$b is:".$a."<br>";
+$a%=$b;
+echo"The value of $a%=$b is:".$a."<br>";
+$a.="day in the month of June";
+echo" The value of $a is:".$a."<br>";
+?>
