6b9fd61d79d3f05deab5422215d2bc16ad0bcb3e
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-06-11 15:32:17 +0700

    Lap

---
+++ PHP/Lap05/Session12/CodeSnippet9.php
@@ -0,0 +1,15 @@
+<?php
+$counter=0;
+while ($counter<5)
+{
+    $counter++;
+    if ($counter==3)
+    {
+        echo"Continues the loop<br>";
+        continue;
+
+    }
+    echo"$counter<br>";
+}
+echo"The loop and here";
+?>
