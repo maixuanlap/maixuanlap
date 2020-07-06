f5c65264c19c05cd0968eb6388919c7f45cdd86a
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-07-06 22:52:16 +0700

    Laravel

---
+++ Laravel/chuong_5/profile.blade.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile</title>
+    </head>
+    <body>
+    <h1>Welcome profile page</h1>
+    <h2>your name {{$name}}</h2>
+    </body>
+</html>
