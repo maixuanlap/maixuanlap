60ca22092a3e30656f33d6453ec404e5551f369c
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-09-28 16:57:38 +0700

    Lap01

---
+++ Sem3/ASP.NET/Lap01/WingtipToys/WingtipToys/Site.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+    }
+
+}
