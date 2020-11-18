378ef096b4c2e1a3a8247c2365e551fa67e9f633
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2020-11-18 22:00:39 +0700

    lap

---
+++ BAI TAP  5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int x ,y;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	int tam =x;
+	x =y;
+	y =tam;
+	printf("ket qua sau khi hoan doi:\nx =%d\ny =%d",x,y);
+	 
+	return 0;
+	}
+	
+	
+	
+	
+	
