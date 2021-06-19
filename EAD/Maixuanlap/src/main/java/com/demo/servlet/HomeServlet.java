96318c260b4fbab8d1f5046b04bfdeed20011c2a
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2021-06-19 17:57:22 +0700

    thi

---
+++ EAD/Maixuanlap/src/main/java/com/demo/servlet/HomeServlet.java
@@ -0,0 +1,47 @@
+package com.demo.servlet;
+
+import com.demo.dao.ProductDao;
+import com.demo.entity.ProductEntity;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
+
+    ProductDao dao = new ProductDao();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            addProduct(request,response);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<ProductEntity> products = dao.getAllProduct();
+        request.setAttribute("products", products);
+        request.getRequestDispatcher("listphone.jsp").forward(request, response);
+
+    }
+
+
+    private void addProduct(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+        String name = request.getParameter("name");
+        int price = Integer.parseInt(request.getParameter("price"));
+        String des = request.getParameter("description");
+        String brand = request.getParameter("brand");
+        ProductEntity product = new ProductEntity(name,price,des,brand);
+        dao.insertProduct(product);
+        response.sendRedirect("product");
+    }
+
+
+}
